8a686426c3dca69285bcdc0d21b0f32f7195a6d0
Sören <soeren.blocksdorf@googlemail.com>
2025-04-26 10:59:28 +0200

    Test: Auto-Deploy Live Update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => res.send('Willkommen auf meinen Seite Sören Bitte Jetzt endlich. mal heute! danke bitte, 10, 11,12, 13, 14,15'));
+app.get('/', (req, res) => res.send('Willkommen auf meinen Seite Sören Bitte Jetzt endlich. mal heute! danke bitte, 10, 11,12, 13, 14,15, 16'));
 
 app.listen(3000, () => {
     console.log('✅ Webhook-Deployment Test v2');
