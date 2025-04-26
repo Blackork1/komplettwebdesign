b0c709998013cda0822850935b70afcb93ea43f8
Sören <soeren.blocksdorf@googlemail.com>
2025-04-26 09:20:57 +0200

    testing

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => res.send('Willkommen auf meinen Seite!'));
+app.get('/', (req, res) => res.send('Willkommen auf meinen Seite Webdesign!'));
 
 app.listen(3000, () => {
   console.log('✅ Server läuft auf Port 3000');
