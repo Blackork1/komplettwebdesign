5f96ac3a6a58b73836d805049ba2aeea2034c6d8
Sören <soeren.blocksdorf@googlemail.com>
2025-04-28 19:35:52 +0200

    Versuch

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 
 // const pool = new Pool({
 //     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
+//     user: process.env.DB_USER,ss
 //     password: process.env.DB_PASSWORD,
 //     database: process.env.DB_NAME,
 //     port: process.env.DB_PORT,
