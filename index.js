5b248e29e15473a6b95e72972a6c5bbf6d83af32
Sören <soeren.blocksdorf@googlemail.com>
2025-05-04 12:25:26 +0200

    test

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
   });
 // const pool = new Pool({ 
-//     connectionString: process.env.DATABASE_URL, testing neuer 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+//     connectionString: process.env.DATABASE_URL, testing neuer 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
 //     ssl: false
 // });
 
