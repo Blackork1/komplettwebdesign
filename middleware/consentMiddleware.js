17ffc45da1c9304668d5e310ebc4f3e2595cb4a3
Sören <soeren.blocksdorf@googlemail.com>
2025-08-10 18:31:42 +0200

    try to fix consent

--- middleware/consentMiddleware.js
+++ middleware/consentMiddleware.js
@@ -1,17 +1,10 @@
 export default (req, res, next) => {
-  const consent = req.session?.cookieConsent ?? {
-    necessary: true,
-    analytics: false,
-    marketing: false
-  };
-
-  // Für Templates angenehm:
-  res.locals.session = req.session ?? {};
-  res.locals.consent = consent;
-  res.locals.gaEnabled = Boolean(consent.analytics);
-
-  // Seiten nicht CDN-cachen lassen, wenn du auf Nummer sicher gehen willst:
-  res.setHeader('Cache-Control', 'private, must-revalidate');
-
-  next();
+    // Wenn noch kein Consent gesetzt: Default { analytics: false, marketing: false }
+    const consent = req.session?.cookieConsent ?? {
+        necessary: true,
+        analytics: false,
+        marketing: false
+    };    res.locals.gaEnabled = Boolean(consent.analytics);
+    res.locals.session = req.session ?? {};
+    next();
 };
