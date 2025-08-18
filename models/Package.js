c89dc5fd9c4cd2f49ba47140386257d7847df0e6
Sören <soeren.blocksdorf@googlemail.com>
2025-08-18 14:50:37 +0200

    fixed package bug on main page to show again and lowered min-width to 240px

--- models/Package.js
+++ models/Package.js
@@ -3,7 +3,7 @@ import pool from '../util/db.js';
 export default class Package {
   static async fetchAll() {
     const pkgRes = await pool.query(
-      `SELECT id,name,price_amount_cents,description,detailshero,stripe_price_id_once
+      `SELECT id,name,price_amount_cents,description,detailshero,stripe_price_id_once,display
        FROM packages ORDER BY id`
     );
     const featRes = await pool.query(
@@ -11,14 +11,16 @@ export default class Package {
     );
     const featMap = {};
     featRes.rows.forEach(({ package_id, feature }) => {
-      featMap[package_id] = featMap[package_id]||[];
+      featMap[package_id] = featMap[package_id] || [];
       featMap[package_id].push(feature);
     });
+
     return pkgRes.rows.map(pkg => ({
       ...pkg,
-      price: (pkg.price_amount_cents/100).toLocaleString('de-DE',{style:'currency',currency:'EUR'}),
-      features: featMap[pkg.id]||[],
-      priceId: pkg.stripe_price_id_once
+      price: (pkg.price_amount_cents / 100).toLocaleString('de-DE', { style: 'currency', currency: 'EUR' }),
+      features: featMap[pkg.id] || [],
+      priceId: pkg.stripe_price_id_once,
+      display: pkg.display
     }));
   }
 
